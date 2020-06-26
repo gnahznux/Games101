78f76f60449a9f89883ad66a4f281ffc66d14011
张寻 <zhangxun@Mac.local>
2020-06-26 18:43:44 +0800

    push homework

---
+++ hw6/CMakeFiles/RayTracing.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/zhangxun/Documents/project/algorithm/ComputerGraphics/hw6/BVH.cpp" "/Users/zhangxun/Documents/project/algorithm/ComputerGraphics/hw6/CMakeFiles/RayTracing.dir/BVH.cpp.o"
+  "/Users/zhangxun/Documents/project/algorithm/ComputerGraphics/hw6/Renderer.cpp" "/Users/zhangxun/Documents/project/algorithm/ComputerGraphics/hw6/CMakeFiles/RayTracing.dir/Renderer.cpp.o"
+  "/Users/zhangxun/Documents/project/algorithm/ComputerGraphics/hw6/Scene.cpp" "/Users/zhangxun/Documents/project/algorithm/ComputerGraphics/hw6/CMakeFiles/RayTracing.dir/Scene.cpp.o"
+  "/Users/zhangxun/Documents/project/algorithm/ComputerGraphics/hw6/Vector.cpp" "/Users/zhangxun/Documents/project/algorithm/ComputerGraphics/hw6/CMakeFiles/RayTracing.dir/Vector.cpp.o"
+  "/Users/zhangxun/Documents/project/algorithm/ComputerGraphics/hw6/main.cpp" "/Users/zhangxun/Documents/project/algorithm/ComputerGraphics/hw6/CMakeFiles/RayTracing.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/opencv/4.3.0_5/include/opencv4"
+  "Eigen/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
